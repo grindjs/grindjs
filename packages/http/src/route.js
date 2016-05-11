a843e1875c32fdf924449ecad12edfe4bda00309
shnhrrsn <shnhrrsn@gmail.com>
2016-05-11 16:03:41 -0400

    Added route extension to prepend/append middleware

---
+++ packages/http/src/route.js
@@ -0,0 +1,52 @@
+import {Route} from 'express'
+
+var hasExtended = false
+
+export function RouteExtension() {
+	if(hasExtended) {
+		return
+	} else {
+		hasExtended = true
+	}
+
+	Route.prototype.use = function(method, handle, prepend = true) {
+		if(typeof method === 'function') {
+			handle = method
+			method = null
+		}
+
+		if(typeof handle !== 'function') {
+			throw new TypeError('Route.use() requires callback functions but got a ' + (typeof handle));
+		}
+
+		if(!method) {
+			method = Object.keys(this.methods)[0]
+		}
+
+		if(typeof this.methods[method] === 'undefined' || this.stack.length === 0) {
+			throw new Error('Route.use() requires method to be already registered, ' + method + ' is not yet registered.')
+		}
+
+		const LayerClass = this.stack[0].constructor
+
+		const layer = LayerClass('/', { }, handle)
+		layer.method = method
+
+		if(prepend) {
+			this.stack.unshift(layer)
+		} else {
+			this.stack.push(layer)
+		}
+
+		return this
+	}
+
+	Route.prototype.useBefore = function(method, handle) {
+		return this.use(method, handle, true)
+	}
+
+	Route.prototype.useAfter = function(method, handle) {
+		return this.use(method, handle, false)
+	}
+
+}
