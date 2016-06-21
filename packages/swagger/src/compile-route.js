f22b117d10d4b6486a65fa440183f3fed59d9712
shnhrrsn <shnhrrsn@gmail.com>
2016-06-21 18:49:57 -0400

    Rename swagger param in compile-route.js to docs to avoid future conflicts

--- packages/swagger/src/compile-route.js
+++ packages/swagger/src/compile-route.js
@@ -3,7 +3,7 @@ import {parameter as inferParameter} from './inference/parameter'
 import {expandParameters} from './expand-parameters'
 
 export function compileRoute(route, app) {
-	const swagger = !route.extra.isNil ? route.extra.swagger : null
+	const docs = !route.extra.isNil ? route.extra.swagger : null
 	var routePath = route.path
 	var method = route.methods
 
@@ -13,26 +13,26 @@ export function compileRoute(route, app) {
 		method = null
 	}
 
-	if(swagger.isNil || routePath.isNil || method.isNil) {
+	if(docs.isNil || routePath.isNil || method.isNil) {
 		return null
 	}
 
 	method = method.toLowerCase()
 
 	// Expand base parameters
-	swagger.parameters = expandParameters(swagger.parameters)
+	docs.parameters = expandParameters(docs.parameters)
 
 	// Infer route, which will modify parameters
-	routePath = inferRoute(routePath, app, swagger.parameters)
+	routePath = inferRoute(routePath, app, docs.parameters)
 
 	// Infer all parameters
-	for(const parameter of swagger.parameters) {
+	for(const parameter of docs.parameters) {
 		inferParameter(parameter, method)
 	}
 
-	if(swagger.parameters.length === 0) {
-		delete swagger.parameters
+	if(docs.parameters.length === 0) {
+		delete docs.parameters
 	}
 
-	return { routePath, method, swagger }
+	return { routePath, method, swagger: docs }
 }
