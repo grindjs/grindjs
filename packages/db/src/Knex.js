cebc1f3d5a16fe2e3172d692006725b6c1e0fc2d
Shaun <mail@shnhrrsn.com>
2016-07-08 16:36:25 -0400

    Removed group function, as that was illadvised

--- packages/db/src/Knex.js
+++ packages/db/src/Knex.js
@@ -1,5 +1,3 @@
-import {group} from './group'
-
 export function Knex() {
 	var knex = require('knex')
 	knex = knex.apply(module, arguments)
@@ -8,11 +6,6 @@ export function Knex() {
 	knex.Promise.prototype.after = knex.Promise.prototype.asCallback
 	knex.client.Raw.prototype.after = knex.client.Raw.prototype.asCallback
 	knex.client.QueryBuilder.prototype.after = knex.client.QueryBuilder.prototype.asCallback
-
-	// Add non-promise based grouped query support
-	knex.group = function(callback) {
-		return group(this, callback)
-	}
 
 	return knex
 }
