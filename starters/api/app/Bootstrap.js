5d1fcbedcb46ee6775740904d3127037d717d68f
shnhrrsn <shnhrrsn@gmail.com>
2016-07-09 18:31:41 -0400

    Removed error handler as it’s now integrated into Grind natively

--- starters/api/app/Bootstrap.js
+++ starters/api/app/Bootstrap.js
@@ -4,16 +4,11 @@ import {DatabaseProvider} from 'grind-db'
 import {SwaggerProvider} from 'grind-swagger'
 
 import 'App/Providers/RoutesProvider'
-import 'App/Providers/ErrorHandlerProvider'
 
 const app = new Grind()
 
 app.providers.push(DatabaseProvider)
 app.providers.push(SwaggerProvider)
 app.providers.push(RoutesProvider)
-
-// ExpressJS requires error middleware be
-// registered last.
-app.providers.push(ErrorHandlerProvider)
 
 module.exports = app
