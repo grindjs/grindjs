25e4276904409298475fd30716a2e874690f2674
shnhrrsn <shnhrrsn@gmail.com>
2016-07-09 10:26:14 -0400

    QueryBuilder.subset can now handle a single parameter object with limit/offset as keys

--- packages/orm/src/QueryBuilder.js
+++ packages/orm/src/QueryBuilder.js
@@ -3,6 +3,11 @@ import { QueryBuilder as ObjectionQueryBuilder } from 'objection'
 export class QueryBuilder extends ObjectionQueryBuilder {
 
 	subset(limit, offset = 0) {
+		if(typeof limit === 'object') {
+			offset = limit.offset || 0
+			limit = limit.limit
+		}
+
 		return this.limit(limit).offset(offset)
 	}
 
