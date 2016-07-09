5e89d7e798c80d1983876193b74c2ddf0f8966e0
shnhrrsn <shnhrrsn@gmail.com>
2016-07-09 18:19:15 -0400

    Moved migrations and seeds into a database folder

--- starters/api/database/migrations/20160403164009_create_states_table.js
+++ starters/api/database/migrations/20160403164009_create_states_table.js
@@ -1,11 +1,11 @@
-export function up(knex, Promise) {
-	return knex.schema.createTable('states', (table) => {
+export function up(knex) {
+	return knex.schema.createTable('states', table => {
 		table.increments('id').primary()
 		table.string('name', 64)
 		table.string('abbreviation', 2).index()
 	})
 }
 
-export function down(knex, Promise) {
+export function down(knex) {
 	return knex.schema.dropTable('states')
 }
