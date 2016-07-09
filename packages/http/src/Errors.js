c414916355b8b6250879d38fb601ffa00eecbe48
shnhrrsn <shnhrrsn@gmail.com>
2016-07-08 22:59:17 -0400

    Replaced http-errors package with built in ES6+ class based errors

--- packages/http/src/Errors.js
+++ packages/http/src/Errors.js
@@ -1,49 +1,28 @@
-import HttpError from 'http-errors'
+import './Errors/HttpError'
 
-global.HttpError = HttpError.HttpError
+const clientErrors = require('./Errors/HttpClientError')
+const serverErrors = require('./Errors/HttpServerError')
 
-global.BadRequestError = HttpError.BadRequest
-global.UnauthorizedError = HttpError.Unauthorized
-global.PaymentRequiredError = HttpError.PaymentRequired
-global.ForbiddenError = HttpError.Forbidden
-global.NotFoundError = HttpError.NotFound
-global.MethodNotAllowedError = HttpError.MethodNotAllowed
-global.NotAcceptableError = HttpError.NotAcceptable
-global.ProxyAuthenticationRequiredError = HttpError.ProxyAuthenticationRequired
-global.RequestTimeoutError = HttpError.RequestTimeout
-global.ConflictError = HttpError.Conflict
-global.GoneError = HttpError.Gone
-global.LengthRequiredError = HttpError.LengthRequired
-global.PreconditionFailedError = HttpError.PreconditionFailed
-global.RequestEntityTooLargeError = HttpError.RequestEntityTooLarge
-global.RequestURITooLongError = HttpError.RequestURITooLong
-global.UnsupportedMediaTypeError = HttpError.UnsupportedMediaType
-global.RequestedRangeNotSatisfiableError = HttpError.RequestedRangeNotSatisfiable
-global.ExpectationFailedError = HttpError.ExpectationFailed
-global.EnhanceYourCalmError = HttpError.EnhanceYourCalm
-global.UnprocessableEntityError = HttpError.UnprocessableEntity
-global.LockedError = HttpError.Locked
-global.FailedDependencyError = HttpError.FailedDependency
-global.UnorderedCollectionError = HttpError.UnorderedCollection
-global.UpgradeRequiredError = HttpError.UpgradeRequired
-global.PreconditionRequiredError = HttpError.PreconditionRequired
-global.TooManyRequestsError = HttpError.TooManyRequests
-global.RequestHeaderFieldsTooLargeError = HttpError.RequestHeaderFieldsTooLarge
-global.NoResponseError = HttpError.NoResponse
-global.RetryWithError = HttpError.RetryWith
-global.BlockedByWindowsParentalControlsError = HttpError.BlockedByWindowsParentalControls
-global.ClientClosedRequestError = HttpError.ClientClosedRequest
-global.InternalServerErrorError = HttpError.InternalServerError
-global.NotImplementedError = HttpError.NotImplemented
-global.BadGatewayError = HttpError.BadGateway
-global.ServiceUnavailableError = HttpError.ServiceUnavailable
-global.GatewayTimeoutError = HttpError.GatewayTimeout
-global.HTTPVersionNotSupportedError = HttpError.HTTPVersionNotSupported
-global.VariantAlsoNegotiatesError = HttpError.VariantAlsoNegotiates
-global.InsufficientStorageError = HttpError.InsufficientStorage
-global.LoopDetectedError = HttpError.LoopDetected
-global.BandwidthLimitExceededError = HttpError.BandwidthLimitExceeded
-global.NotExtendedError = HttpError.NotExtended
-global.NetworkAuthenticationRequiredError = HttpError.NetworkAuthenticationRequired
+Object.assign(global, clientErrors, serverErrors)
 
+const codesMapping = { }
+
+for(const errors of [ clientErrors, serverErrors ]) {
+	for(const name of Object.keys(errors)) {
+		const error = errors[name]
+		const code = error.representsCode
+
+		if(code === 0) {
+			continue
+		}
+
+		codesMapping[code] = error
+	}
+}
+
+HttpError.make = (code, message) => {
+	return new codesMapping[code](message)
+}
+
+global.HttpError = HttpError
 export const Errors = HttpError
