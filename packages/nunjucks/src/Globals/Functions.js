9fefadcacf219e81e6f4682132cc9b80bbe5c982
shnhrrsn <shnhrrsn@gmail.com>
2016-08-05 14:26:38 -0400

    Added HtmlString + markHtml

--- packages/nunjucks/src/Globals/Functions.js
+++ packages/nunjucks/src/Globals/Functions.js
@@ -1,5 +1,9 @@
+import '../HtmlString'
+
 export function Functions(view) {
 	view.addFunction('route', (name, parameters) => {
 		return view.app.url.route(name, parameters)
 	})
+
+	view.addFunction('markHtml', html => new HtmlString(html))
 }
