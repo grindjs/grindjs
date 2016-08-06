8a1d1a2b94a0fa0c1fb6fa51287921b3bf8cbb50
shnhrrsn <shnhrrsn@gmail.com>
2016-08-06 01:44:10 -0400

    Implemented ScssCompiler.enumerateImports

--- packages/assets/src/Compilers/ScssCompiler.js
+++ packages/assets/src/Compilers/ScssCompiler.js
@@ -1,5 +1,9 @@
 import './Compiler'
+
+import '../Support/FS'
 import '../Support/Require'
+
+import path from 'path'
 
 const sass = Require.optionally('node-sass')
 
@@ -31,6 +35,57 @@ export class ScssCompiler extends Compiler {
 				resolve(result.css)
 			})
 		})
+	}
+
+	async enumerateImports(pathname, callback) {
+		const exists = await FS.exists(pathname)
+
+		if(!exists) {
+			return
+		}
+
+		const contents = await FS.readFile(pathname)
+		const importPaths = [ ]
+
+		contents.toString().replace(/@import\s?([^\s]+);/ig, (_, importPath) => {
+			importPaths.push(importPath)
+		})
+
+		for(let importPath of importPaths) {
+			const dirname = path.dirname(pathname)
+			importPath = importPath.replace(/("|'|url|\(|\))/g, '').trim()
+			let partial = null
+
+			if(importPath.indexOf('/') >= 0) {
+				partial = path.join(dirname, path.dirname(importPath), `_${path.basename(importPath)}`)
+			} else {
+				partial = path.join(dirname, `_${importPath}`)
+			}
+
+			importPath = path.join(dirname, importPath)
+			const ext = path.extname(importPath)
+			const files = [ ]
+
+			if(ext !== '.scss' && ext !== '.sass') {
+				files.push(`${importPath}.scss`)
+				files.push(`${partial}.scss`)
+				files.push(`${importPath}.sass`)
+				files.push(`${partial}.sass`)
+			} else {
+				files.push(importPath)
+			}
+
+			for(const file of files) {
+				const exists = await FS.exists(file)
+
+				if(!exists) {
+					continue
+				}
+
+				await callback(file)
+				break
+			}
+		}
 	}
 
 	mime() {
