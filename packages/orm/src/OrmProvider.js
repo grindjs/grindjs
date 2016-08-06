453a387dcda15ba18577c7ee97b5f6e9580ee21e
shnhrrsn <shnhrrsn@gmail.com>
2016-08-06 12:16:23 -0400

    Updated for new app.db reference

--- packages/orm/src/OrmProvider.js
+++ packages/orm/src/OrmProvider.js
@@ -6,7 +6,7 @@ import { ValidationError } from 'objection'
 
 export function OrmProvider(app) {
 	Model.app(app)
-	Model.knex(app.get('db'))
+	Model.knex(app.db)
 
 	Model.QueryBuilder = QueryBuilder
 	Model.RelatedQueryBuilder = QueryBuilder
