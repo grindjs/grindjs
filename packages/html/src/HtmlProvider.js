484ec282d7dae2ed208f3eabd649375d6e480d1f
shnhrrsn <shnhrrsn@gmail.com>
2016-08-05 20:35:33 -0400

    Updated HtmlProvider to allow for passing in different HtmlBuilder/FormBuilder classes

--- packages/html/src/HtmlProvider.js
+++ packages/html/src/HtmlProvider.js
@@ -1,9 +1,12 @@
 import './HtmlBuilder'
 import './FormBuilder'
 
-export function HtmlProvider(app) {
-	const html = new HtmlBuilder(app)
-	const form = new FormBuilder(app, html)
+export function HtmlProvider(app, htmlBuilderClass, formBuilderClass) {
+	htmlBuilderClass = htmlBuilderClass || HtmlBuilder
+	formBuilderClass = formBuilderClass || FormBuilder
+
+	const html = new htmlBuilderClass(app)
+	const form = new formBuilderClass(app, html)
 
 	app.set('html', html)
 	app.set('form', form)
