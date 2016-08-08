ee61155d7311ef82494a3e70c40066aac21eceb3
shnhrrsn <shnhrrsn@gmail.com>
2016-08-08 00:58:10 -0400

    ViewLoader now supports dot path separators (layout.master will expand to layout/master.njk)

--- packages/nunjucks/src/ViewLoader.js
+++ packages/nunjucks/src/ViewLoader.js
@@ -3,11 +3,11 @@ import Nunjucks from 'nunjucks'
 export class ViewLoader extends Nunjucks.FileSystemLoader {
 
 	getSource(name) {
-		if(!name.endsWith('.njk')) {
-			name += '.njk'
+		if(name.endsWith('.njk')) {
+			name = name.substring(0, name.length - 4)
 		}
 
-		return super.getSource(name)
+		return super.getSource(name.replace(/\./, '/') + '.njk')
 	}
 
 }
