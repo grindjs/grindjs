1c7adecd7bd61e666fd0de37354b8bb428afbbd4
shnhrrsn <shnhrrsn@gmail.com>
2016-08-08 12:50:12 -0400

    Moved ViewLoader to ViewEnviornment to handle cache clearing better

---
+++ packages/view/src/ViewEnvironment.js
@@ -0,0 +1,15 @@
+import Nunjucks from 'nunjucks'
+
+export class ViewEnvironment extends Nunjucks.Environment {
+
+	resolveTemplate(loader, parentName, filename) {
+		Log.comment('testing', parentName, filename)
+
+		if(filename.endsWith('.njk')) {
+			filename = filename.substring(0, filename.length - 4)
+		}
+
+		return super.resolveTemplate(loader, parentName, filename.replace(/\./, '/') + '.njk')
+	}
+
+}
--- packages/view/src/ViewFactory.js
+++ packages/view/src/ViewFactory.js
@@ -1,7 +1,7 @@
 import './Globals/Filters'
 import './Globals/Functions'
 
-import './ViewLoader'
+import './ViewEnvironment'
 
 import Nunjucks from 'nunjucks'
 import fs from 'fs'
@@ -17,12 +17,12 @@ export class ViewFactory {
 
 		this.viewPath = app.paths.base(app.config.get('view.path', 'resources/views'))
 
-		const loader = new ViewLoader(this.viewPath, {
+		const loader = new Nunjucks.FileSystemLoader(this.viewPath, {
 			watch: app.config.get('view.watch', app.env() === 'local'),
 			noCache: app.config.get('view.disable_cache', false)
 		})
 
-		this.nunjucks = new Nunjucks.Environment(loader, {
+		this.nunjucks = new ViewEnvironment(loader, {
 			autoescape: app.config.get('view.autoescape', true),
 			trimBlocks: app.config.get('view.trim_blocks', false),
 			lstripBlocks: app.config.get('view.lstrip_blocks', false),
