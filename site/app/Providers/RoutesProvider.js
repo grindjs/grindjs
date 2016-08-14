788338a2b585b5c15bed86402caf0d3f63c59aeb
shnhrrsn <shnhrrsn@gmail.com>
2016-08-14 14:21:19 -0400

    Removed unnecessary routes

--- site/app/Providers/RoutesProvider.js
+++ site/app/Providers/RoutesProvider.js
@@ -1,15 +1,5 @@
-import 'App/Controllers/HelloController'
-
 export function RoutesProvider(app) {
 
-	app.routes.get('/', (req, res) => {
-		res.render('welcome.njk', {
-			name: 'Grind'
-		})
-	}).as('welcome.show')
-
-	app.routes.group({ prefix: 'hello', controller: HelloController }, routes => {
-		routes.get('/', 'show')
-	})
+	app.routes.get('/', (req, res) => res.render('welcome.njk')).as('welcome.show')
 
 }
