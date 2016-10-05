abcf0ae3470ea4a21317595653e90977ec635725
shnhrrsn <shnhrrsn@gmail.com>
2016-10-05 18:44:11 -0400

    Replaced route() function with url object to allow current request object to be shared with the URL generator

--- packages/nunjucks/src/Globals/Functions.js
+++ packages/nunjucks/src/Globals/Functions.js
@@ -1,9 +1,5 @@
 import '../HtmlString'
 
 export function Functions(view) {
-	view.addFunction('route', (name, parameters) => {
-		return view.app.url.route(name, parameters)
-	})
-
 	view.addFunction('markHtml', html => new HtmlString(html))
 }
--- packages/nunjucks/src/ViewProvider.js
+++ packages/nunjucks/src/ViewProvider.js
@@ -2,6 +2,12 @@ import './ViewFactory'
 
 export function ViewProvider(app) {
 	app.view = new ViewFactory(app)
+
+	app.routes.use((req, res, next) => {
+		res.locals.url = app.url.clone(req)
+		next()
+	})
+
 }
 
 ViewProvider.priority = 30000
