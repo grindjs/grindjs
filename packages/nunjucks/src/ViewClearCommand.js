3bfc824efb85e4118a8368ec391cc911acbaa63f
shnhrrsn <shnhrrsn@gmail.com>
2016-11-11 14:47:45 -0500

    Added view:cache and view:clear to precompile views

---
+++ packages/nunjucks/src/ViewClearCommand.js
@@ -0,0 +1,19 @@
+import { Command } from 'grind-cli'
+
+import './FS'
+
+export class ViewClearCommand extends Command {
+	name = 'view:clear'
+	description = 'Clears precompiled view cache'
+
+	async run() {
+		const exists = FS.exists(this.app.view.compiledViewPath)
+
+		if(exists) {
+			await FS.unlink(this.app.view.compiledViewPath)
+		}
+
+		Log.success('Done')
+	}
+
+}
