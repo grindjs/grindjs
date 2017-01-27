2bfc5fb12d45e4d363fa395bc1dd20082de6381a
shnhrrsn <shnhrrsn@gmail.com>
2017-01-26 23:53:12 -0500

    Added active indicator to docs nav

--- site/app/Controllers/DocsController.js
+++ site/app/Controllers/DocsController.js
@@ -13,9 +13,17 @@ export class DocsController extends Controller {
 
 		path = this.app.paths.base('docs', path)
 
-
 		return Promise.all([
-			Markdown.renderFile(this.app, this.app.paths.base('docs/guides/documentation.markdown')),
+			Markdown.renderFile(this.app, this.app.paths.base('docs/guides/documentation.markdown')).then(content => {
+				const components = path.split(/\//)
+				const active = components[components.length - 1]
+
+				return content.replace(
+					new RegExp(`<a href="(${active})">`),
+					'<a href="$1" class="docs-navigation-active">'
+				)
+			}),
+
 			Markdown.renderFile(this.app, `${path}.markdown`)
 		]).then(data => res.render('docs.show', {
 			documentation: data[0],
