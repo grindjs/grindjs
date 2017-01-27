7f42f45ef192bf7cb790ad7c0da114ca254f4fbb
shnhrrsn <shnhrrsn@gmail.com>
2017-01-26 23:27:26 -0500

    Added proper nunjucks syntax highlighting

--- site/app/Support/Highlighter.js
+++ site/app/Support/Highlighter.js
@@ -3,13 +3,25 @@
 import Highlights from 'highlights'
 
 const highlighter = new Highlights()
+
 highlighter.requireGrammarsSync({
 	modulePath: require.resolve('language-json5/package.json')
 })
 
+highlighter.requireGrammarsSync({
+	modulePath: require.resolve('atom-jinja2/package.json')
+})
+
 export function Highlighter(code, lang) {
-	if(lang === 'json') {
-		lang = 'json5'
+	switch(lang) {
+		case 'json':
+			lang = 'json5'
+			break
+		case 'nunjucks':
+		case 'njk':
+		case 'view':
+			lang = 'jinja'
+			break
 	}
 
 	return highlighter.highlightSync({
