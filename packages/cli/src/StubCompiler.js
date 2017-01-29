e3c6aadf99368b2312bd9f74574c9ee972a2ca94
shnhrrsn <shnhrrsn@gmail.com>
2017-01-29 14:55:02 -0500

    Updated StubCompiler to use grind-support

--- packages/cli/src/StubCompiler.js
+++ packages/cli/src/StubCompiler.js
@@ -1,84 +1,35 @@
-import fs from 'fs'
+import { FS } from 'grind-support'
 import dot from 'dot'
-import mkdirp from 'mkdirp'
 import path from 'path'
 
 import './Errors/AbortError'
 
-function compile(stub, context) {
-	return new Promise((resolve, reject) => {
-		try {
-			fs.readFile(stub, (err, stub) => {
-				if(!err.isNil) {
-					reject(err)
-					return
-				}
-
-				stub = stub.toString().replace(/\n/g, '__STUB_LINE').replace(/\t/g, '__STUB_TAB')
-				const compiled = dot.template(stub)(context)
-
-				// eslint-disable-next-line quotes
-				resolve(compiled.replace(/__STUB_LINE/g, "\n").replace(/__STUB_TAB/g, "\t"))
-			})
-		} catch(err) {
-			reject(err)
-		}
-	})
-}
-
-function pathExists(path) {
-	return new Promise(resolve => {
-		fs.stat(path, err => {
-			if(err.isNil) {
-				resolve(true)
-			} else {
-				resolve(false)
-			}
-		})
-	})
-}
-
-function ensurePathExists(path) {
-	return pathExists(path).then(exists => {
-		if(exists) {
-			return
-		}
-
-		return new Promise((resolve, reject) => {
-			mkdirp(path, err => {
-				if(!err.isNil) {
-					reject(err)
-					return
-				}
-
-				resolve()
-			})
-		})
-	})
-}
-
-function writeFile(file, data) {
-	return new Promise((resolve, reject) => {
-		fs.writeFile(file, data, err => {
-			if(!err.isNil) {
-				reject(err)
-				return
-			}
-
-			resolve()
-		})
-	})
-}
-
 export function StubCompiler(stub, target, context) {
-	return pathExists(target).then(exists => {
+	// Check if the target already exists
+	return FS.exists(target).then(exists => {
 		if(!exists) {
 			return
 		}
 
 		throw new AbortError(`${path.relative(process.cwd(), target)} already exists. Refusing to overwrite.`)
 	})
-	.then(() => ensurePathExists(path.dirname(target)))
-	.then(() => compile(stub, context))
-	.then(content => writeFile(target, content))
+
+	// Make sure the path exists
+	.then(() => FS.exists(path.dirname(target))).then(exists => {
+		if(exists) {
+			return
+		}
+
+		return FS.mkdirp(path.dirname(target))
+	})
+
+	// Compile the stub
+	.then(() => FS.readFile(stub)).then(stub => {
+		stub = stub.toString().replace(/\n/g, '__STUB_LINE').replace(/\t/g, '__STUB_TAB')
+
+		return dot.template(stub)(context).replace(/__STUB_LINE/g, '\n').replace(/__STUB_TAB/g, '\t')
+	})
+
+	// Write the target
+	.then(content => FS.writeFile(target, content))
 }
