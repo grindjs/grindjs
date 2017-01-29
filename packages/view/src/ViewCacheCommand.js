7e7bb02537c82e10c91f1ed515985576cab3c455
shnhrrsn <shnhrrsn@gmail.com>
2017-01-29 03:37:22 -0500

    Replaced FS with grind-support

--- packages/view/src/ViewCacheCommand.js
+++ packages/view/src/ViewCacheCommand.js
@@ -1,6 +1,5 @@
 import { Command } from 'grind-cli'
-
-import './FS'
+import { FS } from 'grind-support'
 
 import Nunjucks from 'nunjucks'
 import Path from 'path'
@@ -14,7 +13,7 @@ export class ViewCacheCommand extends Command {
 			env: this.app.view.nunjucks,
 			include: [ /\.njk$/ ],
 			wrapper: templates => {
-				let out = `const templates = { };\nmodule.exports.templates = templates;\n\n`
+				let out = 'const templates = { };\nmodule.exports.templates = templates;\n\n'
 
 				for(const template of templates) {
 					const name = JSON.stringify(template.name)
@@ -30,7 +29,7 @@ export class ViewCacheCommand extends Command {
 
 		if(!(await FS.exists(dir))) {
 			await FS.mkdirp(dir)
-			await FS.writeFile(Path.join(dir, '.gitignore'), `*\n!.gitignore\n`)
+			await FS.writeFile(Path.join(dir, '.gitignore'), '*\n!.gitignore\n')
 		}
 
 		await FS.writeFile(this.app.view.compiledViewPath, result)
