d5d5e99f96959a0d449ae7f2bbc45db1de779d2f
shnhrrsn <shnhrrsn@gmail.com>
2017-01-29 03:56:22 -0500

    Added FS.touch and FS.recursiveReaddir

--- packages/support/src/FS.js
+++ packages/support/src/FS.js
@@ -1,4 +1,5 @@
 import fs from 'fs-promise'
+import recursiveReaddir from 'recursive-readdir'
 
 const FS = { ...fs }
 
@@ -9,6 +10,47 @@ FS.exists = pathname => {
 		}
 
 		return false
+	})
+}
+
+FS.touch = (path, time) => {
+	time = time || Date.now()
+	let fd = null
+
+	return fs.open(path, 'r').then(fdd => {
+		fd = fdd
+	}).then(() => {
+		return fs.futimes(fd, time, time)
+	}).catch(err => {
+		if(!fd.isNil) {
+			return fs.close(fd).then(() => {
+				fd = null
+				throw err
+			})
+		}
+
+		throw err
+	}).then(result => {
+		if(!fd.isNil) {
+			return fs.close(fd).then(() => {
+				fd = null
+				return result
+			})
+		}
+
+		return result
+	})
+}
+
+FS.recursiveReaddir = pathname => {
+	return new Promise((resolve, reject) => {
+		recursiveReaddir(pathname, (err, result) => {
+			if(!err.isNil) {
+				return reject(err)
+			}
+
+			resolve(result)
+		})
 	})
 }
 
