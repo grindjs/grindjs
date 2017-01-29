793577d12699939c605a23ac5da2538bd275dd95
shnhrrsn <shnhrrsn@gmail.com>
2017-01-29 15:12:51 -0500

    Updated make:model command for new changes in grind-cli

--- packages/orm/src/MakeModelCommand.js
+++ packages/orm/src/MakeModelCommand.js
@@ -1,25 +1,29 @@
-import { Command, StubCompiler } from 'grind-cli'
+import { AbortError, Command, InputArgument, InputOption, StubCompiler } from 'grind-cli'
 import './inflect'
 import path from 'path'
 
 export class MakeModelCommand extends Command {
 	name = 'make:model'
 	description = 'Create a model class'
-	arguments = [ 'className?' ]
-	options = {
-		table: 'Name of the table to create the model for'
-	}
+
+	arguments = [
+		new InputArgument('name', InputArgument.VALUE_OPTIONAL, 'The name of the model.')
+	]
+
+	options = [
+		new InputOption('table', InputOption.VALUE_OPTIONAL, 'Name of the table to create the model for')
+	]
 
 	run() {
-		let className = this.argument('className')
+		let name = this.argument('name')
 		let tableName = null
 		let descriptiveName = null
 
 		if(this.containsOption('table')) {
 			tableName = this.option('table')
 
-			if(className.isNil) {
-				className = `${inflect.classify(tableName)}Model`
+			if(name.isNil) {
+				name = `${inflect.classify(tableName)}Model`
 			}
 		}
 
@@ -30,14 +34,13 @@ export class MakeModelCommand extends Command {
 			descriptiveName = inflect.singularize(tableName)
 		}
 
-		if(className.isNil) {
-			this.error('A class name must be provided if `--table` isn’t used.')
-			process.exit(1)
+		if(name.isNil) {
+			throw new AbortError('A class name must be provided if `--table` isn’t used.')
 		}
 
-		const filePath = this.app.paths.app('Models', `${className}.js`)
+		const filePath = this.app.paths.app('Models', `${name}.js`)
 		return StubCompiler(path.join(__dirname, 'stubs', 'Model.stub'), filePath, {
-			className,
+			name,
 			tableName,
 			descriptiveName
 		}).then(() => {
