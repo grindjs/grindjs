f8a9de545734beffdbcffe3ef350674c88cdc631
shnhrrsn <shnhrrsn@gmail.com>
2017-02-01 08:48:34 -0500

    Added tests for merge

---
+++ packages/support/test/merge.js
@@ -0,0 +1,40 @@
+import test from 'ava'
+import '../src/merge'
+
+const lhs = {
+	debug: true,
+	nested: {
+		a: 'a',
+		b: 'b',
+		c: {
+			true: true
+		}
+	},
+	array: [ 'a', 'b' ]
+}
+
+const rhs = {
+	debug: false,
+	nested: {
+		c: {
+			true: false
+		}
+	},
+	array: [ 'c' ]
+}
+
+test('simple', t => {
+	const merged = merge(lhs, rhs)
+	t.is(merged.debug, false)
+})
+
+test('nested', t => {
+	const merged = merge(lhs, rhs)
+	t.is(merged.nested.c.true, false)
+})
+
+test('arrays', t => {
+	const merged = merge(lhs, rhs)
+	t.is(merged.array.indexOf('a'), 0)
+	t.is(merged.array.indexOf('c'), 2)
+})
