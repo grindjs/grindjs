1f17147bd56788b7cd164bb1012bf8b67cf85c70
shnhrrsn <shnhrrsn@gmail.com>
2017-02-01 18:26:06 -0500

    Added tests for UrlGenerator

---
+++ packages/http/test/UrlGenerator.js
@@ -0,0 +1,51 @@
+import test from 'ava'
+
+import './helpers/Grind'
+
+function url() {
+	const app = new Grind({ port: 80 })
+	const handler = (req, res) => res.send(req.path)
+
+	app.routes.get('users', handler).as('users.index')
+	app.routes.get('users/:id', handler).as('users.show')
+
+	return app.url
+}
+
+test('hash', t => {
+	t.is(url().make('#'), 'http://localhost/#')
+	t.is(url().make('#hash'), 'http://localhost/#hash')
+	t.is(url().make('#hash', { }, true), 'https://localhost/#hash')
+})
+
+test('empty', t => {
+	t.is(url().make(''), 'http://localhost/')
+})
+
+test('absolute', t => {
+	t.is(url().make('http://grind.rocks/'), 'http://grind.rocks/')
+})
+
+test('path', t => {
+	t.is(url().make('edit'), 'http://localhost/edit')
+	t.is(url().make('edit', { }, true), 'https://localhost/edit')
+})
+
+test('named', t => {
+	t.is(url().route('users.index'), 'http://localhost/users')
+	t.is(url().route('users.show', 1), 'http://localhost/users/1')
+	t.is(url().route('users.show', [ 1 ]), 'http://localhost/users/1')
+	t.is(url().route('users.show', { id: 1 }), 'http://localhost/users/1')
+	t.is(url().route('users.show', { id: 1 }, true), 'https://localhost/users/1')
+})
+
+test('parameters', t => {
+	t.is(url().make('/', { filter: 'all' }), 'http://localhost/?filter=all')
+	t.is(url().make('/?filter=none', { filter: 'all' }), 'http://localhost/?filter=all')
+	t.is(url().make('/?sort=asc', { filter: 'all' }), 'http://localhost/?sort=asc&filter=all')
+	t.is(url().make('/?sort=asc', { filter: 'all' }, true), 'https://localhost/?sort=asc&filter=all')
+
+	t.is(url().route('users.index', { filter: 'all' }), 'http://localhost/users?filter=all')
+	t.is(url().route('users.show', { id: 1, filter: 'all' }), 'http://localhost/users/1?filter=all')
+	t.is(url().route('users.show', { id: 1, filter: 'all' }, true), 'https://localhost/users/1?filter=all')
+})
