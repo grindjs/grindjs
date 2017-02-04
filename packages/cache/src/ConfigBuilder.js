42ded925b15191620be32c5d7468b15bfef6b329
shnhrrsn <shnhrrsn@gmail.com>
2017-02-04 14:52:34 -0500

    Updated ConfigBuilder to look for a `redis` config file

--- packages/cache/src/ConfigBuilder.js
+++ packages/cache/src/ConfigBuilder.js
@@ -5,20 +5,16 @@ export function ConfigBuilder(store, app) {
 
 	if(store.isNil) {
 		return null
+	} else {
+		store = { ...store }
 	}
 
 	const driver = expandDriverAlias(store.driver || null)
 	delete store.driver
 
-	const config = Object.assign({ }, store)
-
-	if(!config.path.isNil) {
-		config.path = app.paths.base(config.path)
-	}
-
 	return {
 		store: driver.isNil ? 'memory' : require(driver),
-		options: config
+		options: { ...expandStoreConfig(app, driver, store) }
 	}
 }
 
@@ -53,4 +49,47 @@ export function expandDriverAlias(alias) {
 		default:
 			return alias
 	}
+}
+
+export function expandStoreConfig(app, driver, config) {
+	if(!config.path.isNil) {
+		config.path = app.paths.base(config.path)
+	}
+
+	if(driver !== 'cache-manager-redis' || config.connection === void 0) {
+		return config
+	}
+
+	let connection = config.connection
+	delete config.connection
+
+	if(connection === null) {
+		connection = app.config.get('redis.default', null)
+	}
+
+	if(typeof connection === 'string') {
+		connection = app.config.get(`redis.connections.${connection}`)
+	}
+
+	if(connection.isNil) {
+		return config
+	}
+
+	config = { ...config, ...connection }
+	delete config.driver
+
+	if(config.password !== void 0 && config.auth_pass === void 0) {
+		config.auth_pass = config.password
+		delete config.password
+	}
+
+	if(config.host === void 0) {
+		config.host = 'localhost'
+	}
+
+	if(config.port === void 0) {
+		config.port = 6379
+	}
+
+	return config
 }
