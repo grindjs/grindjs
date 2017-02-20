198c400c225ff81977e5ee33aed01adb6396e35e
shnhrrsn <shnhrrsn@gmail.com>
2017-02-20 00:35:57 -0500

    Added req.flashInput() / res.flashInput()

---
+++ packages/http/src/Routing/Extensions/RequestExtension.js
@@ -0,0 +1,15 @@
+const Request = require('express/lib/request.js')
+
+export function RequestExtension() {
+	if(Request._grindHasExtended) {
+		return
+	}
+
+	Request._grindHasExtended = true
+
+	Request.flashInput = function() {
+		this.flash('_old_input', this.body)
+		return this
+	}
+
+}
