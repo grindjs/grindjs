de40c6e4e54a1426fac7ac22c388ade275257d7d
shnhrrsn <shnhrrsn@gmail.com>
2017-03-18 17:02:52 -0400

    Fixed a bug where backslashes in HTML were treated as escape characters [resolves #4]

--- packages/stone/src/Template.js
+++ packages/stone/src/Template.js
@@ -58,12 +58,38 @@ export class Template {
 	 * @return {string}      Sanitized HTML
 	 */
 	static sanitizeHtml(html) {
-		html = html.replace(/(^|[^@])\{\{\s*(.+?)\s*\}\}/g, '$1${escape($2)}')
+		const placeholders = { }
+		let placeholderOrdinal = 0
+
+		// Store regular output blocks
+		html = html.replace(/(^|[^@])\{\{\s*(.+?)\s*\}\}/g, ($0, $1, $2) => {
+			const placeholder = `@@__stone_placeholder_${++placeholderOrdinal}__@@`
+			placeholders[placeholder] = `\${escape(${$2})}`
+			return `${$1}${placeholder}`
+		})
+
+		// Strip escaped braces
 		html = html.replace(/@\{\{(.+?)\}\}/g, '{{$1}}')
-		html = html.replace(/\{!!\s*(.+?)\s*!!\}/g, '${$1}')
+
+		// Store raw output blocks
+		html = html.replace(/\{!!\s*(.+?)\s*!!\}/g, ($0, $1) => {
+			const placeholder = `@@__stone_placeholder_${++placeholderOrdinal}__@@`
+			placeholders[placeholder] = `\${${$1}}`
+			return placeholder
+		})
+
+		// Escape escape characters
+		html = html.replace(/\\/g, '\\\\')
+
+		// Escape whitespace characters
 		html = html.replace(/[\n]/g, '\\n')
 		html = html.replace(/[\r]/g, '\\r')
 		html = html.replace(/[\t]/g, '\\t')
+
+		// Restore placeholders
+		for(const [ placeholder, content ] of Object.entries(placeholders)) {
+			html = html.replace(placeholder, content)
+		}
 
 		return html
 	}
