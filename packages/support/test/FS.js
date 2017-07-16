d2678e6ec8063dc4aaf4fa92689741b03fd1a908
shnhrrsn <shnhrrsn@gmail.com>
2017-07-16 18:03:51 -0400

    Updated FS to take advantage of Node 8’s promisify, if available

---
+++ packages/support/test/FS.js
@@ -0,0 +1,25 @@
+import test from 'ava'
+import '../src/FS'
+
+const path = require('path')
+const fixtures = path.join(__dirname, 'fixtures/fs')
+
+test('read-file', async t => {
+	t.is((await FS.readFile(path.join(fixtures, 'test.txt'))).toString().trim(), 'grind')
+})
+
+test('write-file', async t => {
+	const contents = Date.now().toString()
+	const pathname = path.join(fixtures, 'test-write.txt')
+	await FS.writeFile(pathname, contents)
+	t.is((await FS.readFile(pathname)).toString().trim(), contents)
+})
+
+test('mkdirs', async t => {
+	await FS.mkdirs(path.join(fixtures, `nested/${Date.now()}/a/b/c`))
+	t.pass()
+})
+
+test('exists', async t => {
+	t.is(await FS.exists(path.join(fixtures, 'test.txt')), true)
+})
