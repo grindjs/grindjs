ce7b667ef8a69e3fc236db7c7b3a0e72f92af58e
shnhrrsn <shnhrrsn@gmail.com>
2017-07-23 15:14:46 -0400

    Dropped support for Node 6/7

--- packages/support/src/FS.js
+++ packages/support/src/FS.js
@@ -1,16 +1,58 @@
-let FS = null
+const { promisify } = require('util')
+const fs = require('fs')
 
-if(Number.parseFloat(process.version.substring(1)) >= 8) {
-	FS = { ...require('./FS/Promisify.js').FS }
-} else {
-	FS = { ...require('./FS/Legacy.js').FS }
+export const FS = { }
+
+// Standard fs
+// ----------------------------
+FS.access = promisify(fs.access)
+FS.rename = promisify(fs.rename)
+FS.ftruncate = promisify(fs.ftruncate)
+FS.chown = promisify(fs.chown)
+FS.fchown = promisify(fs.fchown)
+if(fs.lchown) { FS.lchown = promisify(fs.lchown) }
+FS.chmod = promisify(fs.chmod)
+FS.fchmod = promisify(fs.fchmod)
+FS.stat = promisify(fs.stat)
+FS.lstat = promisify(fs.lstat)
+FS.fstat = promisify(fs.fstat)
+FS.link = promisify(fs.link)
+FS.symlink = promisify(fs.symlink)
+FS.readlink = promisify(fs.readlink)
+FS.realpath = promisify(fs.realpath)
+FS.unlink = promisify(fs.unlink)
+FS.rmdir = promisify(fs.rmdir)
+FS.mkdir = promisify(fs.mkdir)
+FS.readdir = promisify(fs.readdir)
+FS.close = promisify(fs.close)
+FS.open = promisify(fs.open)
+FS.utimes = promisify(fs.utimes)
+FS.futimes = promisify(fs.futimes)
+FS.fsync = promisify(fs.fsync)
+FS.fdatasync = promisify(fs.fdatasync)
+FS.write = promisify(fs.write)
+FS.read = promisify(fs.read)
+FS.readFile = promisify(fs.readFile)
+FS.writeFile = promisify(fs.writeFile)
+FS.appendFile = promisify(fs.appendFile)
+FS.truncate = promisify(fs.truncate)
+
+// mkdirp
+// ----------------------------
+let mkdirp = null
+FS.mkdirp = function(...args) {
+	if(mkdirp !== null) {
+		return mkdirp(...args)
+	}
+
+	mkdirp = promisify(require('mkdirp'))
+	return mkdirp(...args)
 }
 
 FS.mkdirs = FS.mkdirp
 
-// Additional functionality
+// exists
 // ----------------------------
-
 FS.exists = pathname => {
 	return FS.stat(pathname).then(() => true).catch(err => {
 		if(err.code !== 'ENOENT') {
@@ -21,6 +63,8 @@ FS.exists = pathname => {
 	})
 }
 
+// touch
+// ----------------------------
 FS.touch = (path, time) => {
 	time = time || Date.now()
 	let fd = null
@@ -50,6 +94,8 @@ FS.touch = (path, time) => {
 	})
 }
 
+// recursiveReaddir
+// ----------------------------
 FS.recursiveReaddir = pathname => {
 	const recursiveReaddir = require('recursive-readdir')
 
@@ -63,5 +109,3 @@ FS.recursiveReaddir = pathname => {
 		})
 	})
 }
-
-export { FS }
