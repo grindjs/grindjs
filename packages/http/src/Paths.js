797fd94d2b628bdb8fb9e741b0cf5a47e359133d
shnhrrsn <shnhrrsn@gmail.com>
2017-07-23 16:10:35 -0400

    Removed find-root dependency

--- packages/http/src/Paths.js
+++ packages/http/src/Paths.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const findRoot = require('find-root')
+const fs = require('fs')
 
 export class Paths {
 	_base = null
@@ -9,8 +9,8 @@ export class Paths {
 	_package = null
 	_public = null
 
-	constructor(bootstrapper, base = null) {
-		this._base = base || findRoot(bootstrapper)
+	constructor(base = null) {
+		this._base = base || this.findBase()
 		this._app = path.join(this._base, 'app')
 		this._config = path.join(this._base, 'config')
 		this._database = path.join(this._base, 'database')
@@ -49,6 +49,32 @@ export class Paths {
 		}
 
 		return rootPath
+	}
+
+	findBase() {
+		if(!process.env.APP_BASE.isNil) {
+			return process.env.APP_BASE
+		}
+
+		const test = /\/pm2|mocha|ava|forever\//i
+		let parent = module
+
+		while(!parent.parent.isNil && !test.test(parent.parent.filename)) {
+			parent = parent.parent
+		}
+
+		let dirname = path.dirname(parent.filename)
+
+		// eslint-disable-next-line no-sync
+		while(dirname !== '/' && dirname !== '.' && !fs.existsSync(path.join(dirname, 'package.json'))) {
+			dirname = path.dirname(dirname)
+		}
+
+		if(dirname === '/' || dirname === '.') {
+			throw new Error('Could not find base path.')
+		}
+
+		return dirname
 	}
 
 }
