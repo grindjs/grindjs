1d3aabdd58b45a55db426665c4e81d12ab332aa1
shnhrrsn <shnhrrsn@gmail.com>
2017-08-23 00:52:53 -0400

    Improvements to RabbitDriver: * ack is now sent _after_ the job is handled, not before * Added support for channel.prefetch to ensure only one job is processed at a time * RabbitDriver.listen() will now resolve when the channel closes * `dispatch` now waits for confirmation before returning

--- packages/queue/src/Drivers/RabbitDriver.js
+++ packages/queue/src/Drivers/RabbitDriver.js
@@ -58,7 +58,7 @@ export class RabbitDriver extends BaseDriver {
 
 	async connect() {
 		this.connection = await amqp.connect(this.uri)
-		this.channel = await this.connection.createChannel()
+		this.channel = await this.connection.createConfirmChannel()
 	}
 
 	async dispatch(job) {
@@ -82,58 +82,76 @@ export class RabbitDriver extends BaseDriver {
 		}
 
 		await this.channel.assertQueue(payload.queue)
-		return this.channel.sendToQueue(payload.queue, new Buffer(JSON.stringify(payload)), options)
+		await this.channel.sendToQueue(payload.queue, new Buffer(JSON.stringify(payload)), options)
+		return this.channel.waitForConfirms()
 	}
 
 	async listen(queues, jobHandler, errorHandler) {
-		const receiver = msg => {
-			const payload = JSON.parse(msg.content)
-			this.channel.ack(msg)
-
-			jobHandler(payload).catch(err => {
-				const tries = Number.parseInt(payload.tries) || 1
-
-				if(tries <= 1) {
-					throw err
-				}
-
-				const options = { ...msg.properties }
-				options.headers = options.headers || { }
-
-				const tryCount = Number.parseInt(options.headers['x-try']) || 1
-
-				if(tryCount >= tries) {
-					throw err
-				} else {
-					options.headers['x-try'] = tryCount + 1
-					options.headers['x-delay'] = Number.parseInt(options.headers['x-retry-delay']) || 0
-				}
-
-				let expiration = Number.parseInt(options.expiration) || 0
-				const timestamp = Number.parseInt(options.timestamp)
-
-				if(expiration > 0) {
-					expiration -= Date.now() - timestamp
-
-					if(expiration <= 0) {
-						throw err
-					}
-
-					options.expiration = expiration
-					options.timestamp = Date.now()
-				}
-
-				return this.channel.publish(msg.fields.exchange, msg.fields.routingKey, msg.content, options)
-			}).catch(err => errorHandler(payload, err))
-		}
+		await this.channel.prefetch(1, queues.length > 1)
 
 		for(const queue of queues) {
 			await this.channel.assertQueue(queue)
 		}
 
+		const receiver = this._receiveMessage.bind(this, jobHandler, errorHandler)
 		await Promise.all(queues.map(queue => this.channel.consume(queue, receiver)))
 
-		return new Promise(() => { })
+		return new Promise((resolve, reject) => {
+			this.channel.on('close', resolve)
+			this.channel.on('error', reject)
+		})
+	}
+
+	async _receiveMessage(jobHandler, errorHandler, msg) {
+		const payload = JSON.parse(msg.content)
+
+		try {
+			await jobHandler(payload)
+		} catch(err) {
+			try {
+				await this._retryMessageOrRethrow(msg, payload, err)
+			} catch(err2) {
+				await errorHandler(payload, err)
+			}
+		} finally {
+			this.channel.ack(msg)
+		}
+	}
+
+	async _retryMessageOrRethrow(msg, payload, err) {
+		const tries = Number.parseInt(payload.tries) || 1
+
+		if(tries <= 1) {
+			throw err
+		}
+
+		const options = { ...msg.properties }
+		options.headers = options.headers || { }
+
+		const tryCount = Number.parseInt(options.headers['x-try']) || 1
+
+		if(tryCount >= tries) {
+			throw err
+		} else {
+			options.headers['x-try'] = tryCount + 1
+			options.headers['x-delay'] = Number.parseInt(options.headers['x-retry-delay']) || 0
+		}
+
+		let expiration = Number.parseInt(options.expiration) || 0
+		const timestamp = Number.parseInt(options.timestamp)
+
+		if(expiration > 0) {
+			expiration -= Date.now() - timestamp
+
+			if(expiration <= 0) {
+				throw err
+			}
+
+			options.expiration = expiration
+			options.timestamp = Date.now()
+		}
+
+		return this.channel.publish(msg.fields.exchange, msg.fields.routingKey, msg.content, options)
 	}
 
 	async destroy() {
