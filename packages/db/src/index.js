d7293dc6c50c69d7ec5780eea5a4f870a6cbdeb1
shnhrrsn <shnhrrsn@gmail.com>
2018-03-10 12:59:29 -0500

    Removed legacy aliases

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -2,15 +2,8 @@ import './ConfigBuilder'
 import './DatabaseBuilder'
 import './DatabaseProvider'
 
-const Config = ConfigBuilder
-const Database = DatabaseBuilder
-
 export {
 	ConfigBuilder,
 	DatabaseBuilder,
-	DatabaseProvider,
-
-	// Legacy names
-	Config,
-	Database
+	DatabaseProvider
 }
