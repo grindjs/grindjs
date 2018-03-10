ec35fa88f4a9a512e8bdcc32d9298ede0e59557f
shnhrrsn <shnhrrsn@gmail.com>
2018-03-10 14:09:42 -0500

    Upgraded to grind-framework 0.8.0-beta.1

--- packages/stone/test/fixtures/makeApp.js
+++ packages/stone/test/fixtures/makeApp.js
@@ -1,20 +1,21 @@
 import '../../src/StoneEngine'
 
-import { Grind, Paths as BasePaths } from 'grind-framework'
+import { Application, Paths as BasePaths } from 'grind-framework'
+import { HttpKernel } from 'grind-http'
 import { ViewProvider, ViewFactory } from 'grind-view'
 
 ViewFactory.engines.stone = (...args) => new StoneEngine(...args)
 
 class Paths extends BasePaths {
 
-	constructor(bootstrapper) {
-		super(bootstrapper, __dirname)
+	constructor() {
+		super(__dirname)
 	}
 
 }
 
 export async function makeApp(before = () => { }) {
-	const app = new Grind({ pathsClass: Paths })
+	const app = new Application(HttpKernel, { pathsClass: Paths })
 	app.config.set('view.ignore-compiled', true)
 	app.providers.add(ViewProvider)
 	before(app)
