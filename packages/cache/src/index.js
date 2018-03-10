a5db3486118edba2c457f9f1d0ca63f128c11b7c
shnhrrsn <shnhrrsn@gmail.com>
2018-03-10 13:19:24 -0500

    Removed exported legacy alias

--- packages/cache/src/index.js
+++ packages/cache/src/index.js
@@ -2,16 +2,8 @@ import './CacheProvider'
 import './ConfigBuilder'
 import './CacheBuilder'
 
-function Config(...args) {
-	Log.error('WARNING: Config has been renamed to ConfigBuilder and will be removed in 0.7')
-	return ConfigBuilder(...args)
-}
-
 export {
 	CacheBuilder,
 	CacheProvider,
 	ConfigBuilder,
-
-	// Legacy
-	Config
 }
