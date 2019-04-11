7453ca4b69329c3f16a158b5ea2fadbd27dd6f7f
shnhrrsn <shnhrrsn@gmail.com>
2019-04-11 16:26:39 -0400

    Updated for Grind 0.8 changes

--- packages/validation/src/ValidationProvider.js
+++ packages/validation/src/ValidationProvider.js
@@ -6,8 +6,12 @@ export function ValidationProvider(app, validatorClass, validationErrorHandler) 
 	validatorClass = validatorClass || Validator
 	app.validator = new validatorClass(app, app.config.get('validation', { }))
 
-	app.errorHandler.shouldntReport.push(ValidationError)
-	app.errorHandler.register(ValidationError, validationErrorHandler || ValidationErrorHandler)
+	if(app.http.isNil) {
+		return
+	}
+
+	app.http.errorHandler.shouldntReport.push(ValidationError)
+	app.http.errorHandler.register(ValidationError, validationErrorHandler || ValidationErrorHandler)
 }
 
 ValidationProvider.priority = 21000
