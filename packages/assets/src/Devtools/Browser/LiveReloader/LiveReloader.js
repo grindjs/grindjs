911d8991334326a9c5994873b8c5dd93e4599e5d
shnhrrsn <shnhrrsn@gmail.com>
2019-07-27 12:33:12 -0400

    Updated live reloader to always reload page if there’s a build time error and a file changes

--- packages/assets/src/Devtools/Browser/LiveReloader/LiveReloader.js
+++ packages/assets/src/Devtools/Browser/LiveReloader/LiveReloader.js
@@ -3,7 +3,9 @@ import { JsReloader } from './JsReloader'
 
 export function LiveReloader(context) {
 	context.socket.on('change', pathname => {
-		if(/(css|sass|less|stylus|styl)$/i.test(pathname)) {
+		if(Object.keys(context.errors).length > 0) {
+			return window.location.reload()
+		} else if(/(css|sass|less|stylus|styl)$/i.test(pathname)) {
 			CssReloader(pathname)
 		} else if(/(js|jsx)$/i.test(pathname)) {
 			JsReloader(pathname)
