8a33200574c6aae84a94343192f2c61e6187d4b9
shnhrrsn <shnhrrsn@gmail.com>
2020-04-26 23:43:39 -0400

    test: updated ava config to set timeout=60

--- ava.config.cjs
+++ ava.config.cjs
@@ -2,4 +2,5 @@ module.exports = {
 	require: [require.resolve('./helpers/ava-register')],
 	files: ['test/**/*.js', '!test/fixtures/**/*.js', '!test/helpers/**/*.js'],
 	verbose: true,
+	timeout: '60s',
 }
