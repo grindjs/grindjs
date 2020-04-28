bd6d3afe843f210ce2b4c0f8c20e697cb6287038
shnhrrsn <shnhrrsn@gmail.com>
2020-04-27 21:13:29 -0400

    build: updated build script

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-
-rm -fr lib
-exec babel --root-mode upward -s inline -d lib/ src/ --copy-files
+exec babel --root-mode upward -s inline -d lib/ src/ --copy-files --delete-dir-on-start
