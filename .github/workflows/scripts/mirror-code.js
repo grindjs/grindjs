0733d1c7e5a43197c72a863d90f21b848d1c6c56
shnhrrsn <shnhrrsn@gmail.com>
2020-09-22 01:38:45 -0400

    ci: update mirror workflow to support branches

--- .github/workflows/scripts/mirror-code.js
+++ .github/workflows/scripts/mirror-code.js
@@ -2,11 +2,21 @@
 const log = require('./utils/log')
 const git = require('./utils/git')
 
+const [, branch] = (process.env.GITHUB_REF || '').match(/^refs\/heads\/(.+?)$/) || []
+
+if (!branch) {
+	throw new Error('Branch not detected')
+}
+
 for (const [pathname, { repo }] of Object.entries(require('./mirrors.json'))) {
-	log.info(`Mirroring ${pathname} to ${repo}`)
+	log.info(`Mirroring ${pathname} to ${repo}#${branch}`)
 	const slug = pathname.replace(/[^a-z]+/gi, '-')
 	git('subtree', 'split', '-P', pathname, '-b', slug)
 	git('remote', 'add', slug, repo)
-	git('push', slug, `${slug}:main`, '--force')
-	git('push', slug, `${slug}:master`, '--force')
+
+	if (branch === 'master') {
+		git('push', slug, `${slug}:main`, '--force')
+	}
+
+	git('push', slug, `${slug}:${branch}`, '--force')
 }
