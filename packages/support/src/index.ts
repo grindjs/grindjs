e206589754cdc7ed75aeb0a00e4fed8213b3698c
shnhrrsn <shnhrrsn@gmail.com>
2020-10-20 21:29:09 -0400

    feat(support): remove lazy
    There’s no great way to make lazy type safe and effective in typescript/esm, so dropping
    
    BREAKING CHANGE: lazy is no longer available

--- packages/support/src/index.ts
+++ packages/support/src/index.ts
@@ -1,12 +1,6 @@
-import { lazy } from './lazy'
-
-module.exports = {
-	lazy,
-}
-
-lazy(module.exports, 'FS', () => require('./FS').FS)
-lazy(module.exports, 'Inflect', () => require('./Inflect').Inflect)
-lazy(module.exports, 'merge', () => require('./merge').merge)
-lazy(module.exports, 'Obj', () => require('./Obj').Obj)
-lazy(module.exports, 'Str', () => require('./Str').Str)
-lazy(module.exports, 'Watcher', () => require('./Watcher').Watcher)
+export { FS } from './FS'
+export { Inflect } from './Inflect'
+export { merge } from './merge'
+export { Obj } from './Obj'
+export { Str } from './Str'
+export { Watcher } from './Watcher'
