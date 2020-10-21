3142584a44af9f53125292a657fa0662d37ca090
shnhrrsn <shnhrrsn@gmail.com>
2020-10-20 21:25:40 -0400

    refactor(support): inline typing for Inflect

--- packages/support/src/Inflect.ts
+++ packages/support/src/Inflect.ts
@@ -1,3 +1,17 @@
-import i from 'i/lib/methods'
+const i = require('i/lib/methods')
 
-export const Inflect = { ...i }
+export const Inflect: {
+	camelize(lowerCaseAndUnderscoredWord: string, firstLetterInUppercase: string): string
+	classify(tableName: string): string
+	dasherize(underscoredWord: string): string
+	demodulize(classNameInModule: string): string
+	foreign_key(className: string, separateClassNameAndIdWithUnderscore: string): string
+	humanize(lowerCaseAndUnderscoredWord: string): string
+	ordinalize(number: string): string
+	pluralize(word: string): string
+	singularize(word: string): string
+	tableize(className: string): string
+	titleize(word: string): string
+	uncountability(word: string): string
+	underscore(camelCasedWord: string): string
+} = { ...i }
